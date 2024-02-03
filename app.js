eb388da9e09579e74803e7da467960edc743615d
warrant <warrant1997@gmail.com>
2024-02-03 14:39:24 +1100

    feat: create sipmle schema for event and events

--- app.js
+++ app.js
@@ -1,10 +1,62 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(bodyParser.json());
-app.listen(3000);
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
-});
+const events = [];
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      } 
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      type RootQuery {
+        events: [Event!]!
+      }
+
+      type RootMutation {
+        createEvent(eventInput: EventInput): Event
+      }
+    
+      schema{
+        query: RootQuery ,
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(3000);
